dae54e6818689337ac201608d29afa7139c75d59
Carlos Henrique <carlosh.carmo@hotmail.com>
2021-09-04 12:08:01 -0300

    criado routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { CadastrarCandidatoComponent } from './cadastrar-candidato/cadastrar-candidato.component';
 import { CadastrarEmpresaComponent } from './cadastrar-empresa/cadastrar-empresa.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
 
-    {path: '', redirectTo: 'entrar', pathMatch: 'full'},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path:'home', component: HomeComponent},
     {path:'entrar', component: EntrarComponent},
     {path:'cadastrar-empresa', component: CadastrarEmpresaComponent},
     {path:'cadastrar-candidato', component: CadastrarCandidatoComponent},
     {path:'inicio', component: InicioComponent}
 ];
+
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
-})
-export class AppRoutingModule { }
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
