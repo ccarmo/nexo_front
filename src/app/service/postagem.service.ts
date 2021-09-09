906b8fdefa57ffd59edf73fc55c5c2a430890bae
felipe0059 <felipe.nx4@gmail.com>
2021-09-09 11:26:21 -0300

    Token Postagem

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostagemService {
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  constructor() { }
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token),
+  };
+
+  Novapostagem (postagem: Postagem): Observable<Postagem> {
+    
+    return this.http.put<Postagem>('http://localhost:8080​/postagem/novapostagem', Postagem);
 }
+}
+
