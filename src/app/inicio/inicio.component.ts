f8338c168e98fa0ca5708bf99c8a5a90652c47ef
byankaa <biancaadecampos123@gmail.com>
2021-09-09 14:31:59 -0300

    configuracao inicial

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
 import { Usuario } from '../model/Usuario';
 import { AuthService } from '../service/auth.service';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -16,16 +20,27 @@ export class InicioComponent implements OnInit {
   nome = environment.nome;
   foto = environment.foto;
   email = environment.email;
+  postagem: Postagem = new Postagem()
+  listaPostagens: Postagem[]
+  tituloPost: string
+
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+  idTema: number
+  nomeTema: string
 
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router, 
+    private postagemService: PostagemService,
+    private temaService: TemaService
   ) { }
 
   ngOnInit() {
     if(environment.token == ''){
       alert('Sua seção expirou, faça o login novamente.')
       this.router.navigate(['/entrar'])
+      
     }
   }
 
--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -16,9 +16,26 @@ export class PostagemService {
     headers: new HttpHeaders().set('Authorization', environment.token),
   };
 
-  Novapostagem (postagem: Postagem): Observable<Postagem> {
-    
-    return this.http.put<Postagem>('http://localhost:8080​/postagem/novapostagem', Postagem);
-}
-}
+  novaPostagem(postagem: Postagem): Observable<Postagem> {
 
+    return this.http.put<Postagem>('http://localhost:8080​/postagem/novapostagem', Postagem);
+  }
+
+  getByIdPostagem(id: number): Observable<Postagem> {
+    return this.http.get<Postagem>('http://localhost:8080/postagem/id/{id}', this.token)
+  }
+
+  postPostagem(postagem: Postagem): Observable<Postagem> {
+    return this.http.post<Postagem>('http://localhost:8080/postagem/novapostagem', this.token)
+  }
+
+  putPostagem(postagem: Postagem): Observable<Postagem> {
+    return this.http.put<Postagem>('http://localhost:8080​/postagem​/alterarpostagem', Postagem, this.token)
+  }
+
+  deletePostagem(id: number) {
+    return this.http.delete(`http://localhost:8080/postagens/id/{id}`, this.token)
+  }
+
+
+}
