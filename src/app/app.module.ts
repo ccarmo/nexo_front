2e85eb5b47f8428fb1f6e2e8a40eb15dad4c0b55
felipe0059 <felipe.nx4@gmail.com>
2021-09-12 11:05:21 -0300

    Adequação final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,8 @@ import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
 import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
 import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
+import { UtilidadesComponent } from './utilidades/utilidades.component';
+import { TestemunhosComponent } from './testemunhos/testemunhos.component';
 
 
 
@@ -42,6 +44,8 @@ import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delet
     TemaDeleteComponent,
     PostagemEditComponent,
     PostagemDeleteComponent,
+    UtilidadesComponent,
+    TestemunhosComponent,
 
   ],
   imports: [
---
+++ src/app/testemunhos/testemunhos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testemunhos',
+  templateUrl: './testemunhos.component.html',
+  styleUrls: ['./testemunhos.component.css']
+})
+export class TestemunhosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
